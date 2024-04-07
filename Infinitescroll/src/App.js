d501d889144e44a6501023e4d3d730df59ab4479
LAPTOP-AQ7BUTEI\NiteshRathore <nitesh.rathore@kibbcom.in>
2024-04-07 11:24:00 +0530

    added infinite scroll

---
+++ Infinitescroll/src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import InfiniteScroll from "./InfiniteScroll";
+export default function App() {
+  return (
+    <div className="App">
+      <InfiniteScroll />
+    </div>
+  );
+}
---
+++ Infinitescroll/src/InfiniteScroll.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+const InfiniteScroll = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?page=${page}`
+      );
+      const incomingData = await response.json();
+      setData((prev) => [...prev, ...incomingData]);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error.message);
+    }
+  };
+
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 10 && !loading) {
+      setLoading(true);
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <section className="landing">
+      <div className="row">
+        <h1 className="landing-heading">Infinite Scroll:</h1>
+        {data &&
+          data.map((item) => (
+            <div key={item.id}>
+              <h3 className="heading">Title: {item.title}</h3>
+            </div>
+          ))}
+        {loading && <p className="loading">Loading...</p>}
+      </div>
+    </section>
+  );
+};
+
+export default InfiniteScroll;
